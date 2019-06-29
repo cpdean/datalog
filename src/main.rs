d02ac1600fe9621c3810f5fe5207a6fe0a487d9a
Conrad Dean <conrad.p.dean@gmail.com>
2019-06-28 21:00:07 -0400

    some parsers

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,98 @@
+#![allow(unused_imports)]
+
+#[macro_use]
+extern crate nom;
+
+use nom::{IResult,Err};
+use nom::error::ErrorKind;
+
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
-use nom::alpha;
-use nom::{complete, delimited, do_parse, named, one_of, tag, ws};
+use regex::Regex;
 
-named!(syntactic_keyword, tag!("else"));
 
-named!(lowercase_char<char>, one_of!("abcdefghijklmnopqrstuvwxyz"));
-named!(uppercase_char<char>, one_of!("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
 
-named!(identifier<&str>, do_parse!(lowercase_char, alpha));
-
-// fact := <ident>(<ident>)
-//ident, tag!("("), ident, tag!(")")
-//named!(fact, ws!(delimited!(ident, tag!("("), ident, tag!(")"))));
-
-fn parse(line: &str) {
-    let res = syntactic_keyword(line.as_bytes());
-    println!("Parsed {:#?}", res);
+fn eval<T>(i: T) -> T {
+    i
 }
 
-fn eval(i: String) -> String {
-    parse(&i);
-    i
+
+/*
+macro_rules! re_capture (
+  ($i:expr, $re:expr) => (
+    {
+      use $crate::lib::std::result::Result::*;
+      use $crate::{Err,error::ErrorKind,IResult};
+
+      use $crate::Slice;
+      let re = $crate::lib::regex::Regex::new($re).unwrap();
+      if let Some(c) = re.captures(&$i) {
+        let v:Vec<_> = c.iter().filter(|el| el.is_some()).map(|el| el.unwrap()).map(|m| $i.slice(m.start()..m.end())).collect();
+        let offset = {
+          let end = v.last().unwrap();
+          end.as_ptr() as usize + end.len() - $i.as_ptr() as usize
+        };
+        Ok(($i.slice(offset..), v))
+      } else {
+        let res: IResult<_,_> = Err(Err::Error(error_position!($i, ErrorKind::RegexpCapture)));
+        res
+      }
+    }
+  )
+);
+*/
+
+
+
+
+/*
+fn find_a(i: &str) -> IResult<&str, &str> {
+    let r = Regex::new("(a)").unwrap();
+    match r.captures(&i) {
+        Some(caps) => {
+            Ok((&i[0..1], &i[0..1]))
+        }
+        None => Err(ErrorKind::RegexpCapture)
+
+    }
+}
+
+*/
+
+
+fn find_a(i: &str) -> IResult<&str, &str> {
+    let found = &i[0..1];
+    let rest = &i[1..2];
+    Ok((rest, found))
+}
+
+fn free_var(i: &str) -> IResult<&str, &str> {
+    let re = Regex::new(r"^[A-Z]+\w*").unwrap();
+    match re.find(i) {
+        Some(m) => {
+            let (s, e) = (m.start(), m.end());
+            Ok((&i[e..], &i[s..e]))
+        },
+        None => {
+            let res: IResult<_,_> = Err(Err::Error(error_position!(i, ErrorKind::RegexpCapture)));
+            res
+        }
+    }
+}
+
+fn identifier(i: &str) -> IResult<&str, &str> {
+    let re = Regex::new(r"^[a-z]+\w*").unwrap();
+    match re.find(i) {
+        Some(m) => {
+            let (s, e) = (m.start(), m.end());
+            Ok((&i[e..], &i[s..e]))
+        },
+        None => {
+            let res: IResult<_,_> = Err(Err::Error(error_position!(i, ErrorKind::RegexpCapture)));
+            res
+        }
+    }
 }
 
 fn main() {
@@ -56,4 +126,37 @@ fn main() {
         }
     }
     //rl.save_history("history.txt").unwrap();
+}
+
+#[test]
+fn supfam(){
+    let a = find_a("ab");
+    assert_eq!(Ok(("b", "a")), a);
+}
+
+#[test]
+fn test_free_var(){
+    assert_eq!(Ok(("", "Za")), free_var("Za"));
+    assert_eq!(Ok((" goat", "Za")), free_var("Za goat"));
+    assert_eq!(Ok((" goat", "YUS")), free_var("YUS goat"));
+    assert_eq!(Err(Err::Error(("yus goat", ErrorKind::RegexpCapture))), free_var("yus goat"));
+}
+
+#[test]
+fn test_identifier(){
+    assert_eq!(Ok(("", "za")), identifier("za"));
+    assert_eq!(Ok((" goat", "za")), identifier("za goat"));
+    assert_eq!(Err(Err::Error(("YUS goat", ErrorKind::RegexpCapture))), identifier("YUS goat"));
+}
+
+#[test]
+fn ugh(){
+    let re = Regex::new(r"^[A-Z]+\w*").unwrap();
+    assert_eq!(true, re.is_match("Aa"));
+    assert_eq!(false, re.is_match(" Aa"));
+    assert_eq!(true, re.is_match("Za"));
+    assert_eq!(true, re.is_match("ZZa"));
+    assert_eq!(true, re.is_match("Zaa"));
+    assert_eq!(true, re.is_match("Zaa "));
+    assert_eq!(true, re.is_match("Z"));
 }
