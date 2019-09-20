c541cff0ade3e788d869774c8ba5e1cfb32c2116
Conrad Dean <conrad.p.dean@gmail.com>
2019-09-20 14:58:09 -0400

    stub test for sqlite

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,11 @@
-#![allow(unused_imports,dead_code)]
+#![allow(unused_imports, dead_code)]
+extern crate rusqlite;
+extern crate time;
+
+use rusqlite::types::ToSql;
+use rusqlite::{params, Connection, Result as SQLResult};
+use time::Timespec;
+
 /*
  * stores the datalog facts and lets you query them
  */
@@ -42,12 +49,68 @@ impl Engine {
 
 #[test]
 fn single_check() {
-    let mut e = Engine{ facts: vec![], rules: vec![] };
-    e.push(Statement::Fact(
-        Fact{ name: "foo".to_owned(), vars: vec![Fixed("bar".to_owned())]}
-    )).unwrap();
-    let r = e.query(
-        &Fact{ name: "foo".to_owned(), vars: vec![Fixed("bar".to_owned())]}
-    );
+    let mut e = Engine {
+        facts: vec![],
+        rules: vec![],
+    };
+    e.push(Statement::Fact(Fact {
+        name: "foo".to_owned(),
+        vars: vec![Fixed("bar".to_owned())],
+    }))
+    .unwrap();
+    let r = e.query(&Fact {
+        name: "foo".to_owned(),
+        vars: vec![Fixed("bar".to_owned())],
+    });
     assert_eq!(r.len(), 1);
+}
+
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    time_created: Timespec,
+    data: Option<Vec<u8>>,
+}
+
+#[test]
+fn test_things() -> SQLResult<()> {
+    let conn = Connection::open_in_memory()?;
+
+    conn.execute(
+        "CREATE TABLE person (
+                  id              INTEGER PRIMARY KEY,
+                  name            TEXT NOT NULL,
+                  time_created    TEXT NOT NULL,
+                  data            BLOB
+                  )",
+        params![],
+    )?;
+    let me = Person {
+        id: 0,
+        name: "Steven".to_string(),
+        time_created: time::get_time(),
+        data: None,
+    };
+    conn.execute(
+        "INSERT INTO person (name, time_created, data)
+                  VALUES (?1, ?2, ?3)",
+        params![me.name, me.time_created, me.data],
+    )?;
+
+    let mut stmt = conn.prepare("SELECT id, name, time_created, data FROM person")?;
+    let person_iter = stmt.query_map(params![], |row| {
+        Ok(Person {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            time_created: row.get(2)?,
+            data: row.get(3)?,
+        })
+    })?;
+
+    let names: Vec<String> = person_iter.map(|p| p.unwrap().name).collect();
+
+    assert_eq!(vec!["Steven"], names);
+
+    Ok(())
 }
